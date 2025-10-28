9cece13a1731de456537a0ee540be3499b5c0976
mtyszczak <mateusz.tyszczak@gmail.com>
2025-10-28 16:43:56 +0100

    Bump dependencies & 3rd party API definitions

--- src/utils/wallet/ctokens/api/index.js
+++ src/utils/wallet/ctokens/api/index.js
@@ -1,37 +1,37 @@
 export default {
   ctokensApi: {
-    urlPath: 'ctokens-api',
+    urlPath: "ctokens-api",
     version: {
-      urlPath: 'version',
-      method: 'GET'
+      urlPath: "version",
+      method: "GET"
     },
     lastSyncedBlock: {
-      urlPath: 'last-synced-block',
-      method: 'GET'
+      urlPath: "last-synced-block",
+      method: "GET"
     },
     topHolders: {
-      urlPath: 'top-holders',
-      method: 'GET'
+      urlPath: "top-holders",
+      method: "GET"
     },
     balances: {
-      urlPath: 'balances',
-      method: 'GET'
+      urlPath: "balances",
+      method: "GET"
     },
     balanceHistory: {
-      urlPath: 'balance-history',
-      method: 'GET'
+      urlPath: "balance-history",
+      method: "GET"
     },
     registeredTokens: {
-      urlPath: 'registered-tokens',
-      method: 'GET'
+      urlPath: "registered-tokens",
+      method: "GET"
     },
     registeredUsers: {
-      urlPath: 'registered-users',
-      method: 'GET'
+      urlPath: "registered-users",
+      method: "GET"
     },
     status: {
-      urlPath: 'status',
-      method: 'GET'
+      urlPath: "status",
+      method: "GET"
     }
   }
-};
+}
