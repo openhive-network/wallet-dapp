9fd8ef156997b3d9c89fbcdfe304c1158fb296a9
Bartek Wrona <wrona@syncad.com>
2025-11-26 02:42:46 +0100

    Added ability to specify Google Wallet IssuerID using env.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,6 +62,7 @@ export default defineNuxtConfig({
       snapOrigin: '',
       snapVersion: ''
     },
-    googleApplicationCredentialsJson: {}
+    googleApplicationCredentialsJson: {},
+    googleWalletIssuerId: ''
   }
 });
